af57e8116df3ad9e3745ec7dce89adaef8e9839b
snehal patel <snehal.patel@e-sky.ca>
2022-03-11 13:59:35 -0500

    #2 customizing tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,26 @@
 module.exports = {
   content: ['*'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#00ADB5',
+        secondary: '#393E46',
+        'font-color': '#eee',
+        baclground: '#222831',
+      },
+    },
+    fontFamily: {
+      Poppins: ['"Poppins"', 'sans-serif'],
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        lg: '1124px',
+        xl: '1124px',
+        '2xl': '1124px',
+      },
+    },
   },
   plugins: [],
 };
